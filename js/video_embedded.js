4bba896a8d4e709282f58fb2958265bfc2c218ed
ishaq9754 <mohdishaq9213@gmail.com>
2024-12-16 15:06:58 +0530

    ifram delet

--- js/video_embedded.js
+++ js/video_embedded.js
@@ -1,18 +1,18 @@
-$(function () {
-    var videoBtn = $('.video-btn');
-
-    videoBtn.click(function () {
-        var videoContainer = $(this).closest('.video-container');
-        var src = videoContainer.data('url');
-        var title = videoContainer.data('title');
-        var videoFrame = $('<iframe>', {
-            src: 'https://www.youtube.com/embed/AZG1mgpKIg0?si=XeY8V7siy-Z934cy&amp;controls=1',
-            frameborder: 0,
-            title: title,
-            allow : "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ,
-            class: 'video-iframe'
-        });
-
-        videoContainer.append(videoFrame);
+// JavaScript to handle the click event and load the iframe
+document.addEventListener('DOMContentLoaded', function() {
+    const youtubeFacade = document.querySelector('.youtube-facade');
+    const youtubeVideoId = 'https://www.youtube.com/embed/AZG1mgpKIg0?si=XeY8V7siy-Z934cy&amp;controls=1';
+  
+    youtubeFacade.addEventListener('click', function() {
+      const iframe = document.createElement('iframe');
+      iframe.src = `https://www.youtube.com/embed/AZG1mgpKIg0?si=XeY8V7siy-Z934cy&amp;controls=1?autoplay=1`;
+      iframe.width = '560';
+      iframe.height = '315';
+      iframe.allowFullscreen = true;
+  
+      // Clear the placeholder content and append the iframe
+      youtubeFacade.innerHTML = '';
+      youtubeFacade.appendChild(iframe);
     });
-});
+  });
+  
