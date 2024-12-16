a8222cba1da6c2f17ba758fc1b40299eb90ec728
ishaq9754 <mohdishaq9213@gmail.com>
2024-12-16 14:25:39 +0530

    js

--- js/video_embedded.js
+++ js/video_embedded.js
@@ -6,7 +6,7 @@ $(function () {
         var src = videoContainer.data('url');
         var title = videoContainer.data('title');
         var videoFrame = $('<iframe>', {
-            src: src,
+            src: 'https://www.youtube.com/embed/AZG1mgpKIg0?si=XeY8V7siy-Z934cy&amp;controls=1',
             frameborder: 0,
             title: title,
             allow : "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ,
